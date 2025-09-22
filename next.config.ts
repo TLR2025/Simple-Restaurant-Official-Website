9e036c4af5b8ca50a826119d7aeb513c88582ac9
TLR <liangruitian401@gmail.com>
2025-09-22 10:47:11 +0200

    Fixed the image remotePatterns problem in the deployment.

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
       protocol: 'http',
       hostname: 'localhost',
       port: '3000',
+    },{
+      protocol: 'https',
+      hostname: '15modern-landing-page.vercel.app',
     }], 
   },
 };
